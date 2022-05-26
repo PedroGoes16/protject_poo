e77fa99b563feca1694a9cab6dcaa4a88ae56039
marcelaabreuu <89107948+marcelaabreuu@users.noreply.github.com>
2022-05-26 20:32:46 -0300

    Update LocalizacaoGeografica.h

--- LocalizacaoGeografica.h
+++ LocalizacaoGeografica.h
@@ -8,7 +8,7 @@ class LocalizacaoGeografica {
     // como já estamos chamando o latitude e longitude no construtor, não precisamos de set (prof disse)
     // ele também me disse que não é necessário o nome da variável, apenas seu tipo
     localizacaoGeografica(float, float);
-    void set_latitude();
-    void set_longitude();
+    float get_latitude();
+    float get_longitude();
     ~localizacaoGeografica();
 }
